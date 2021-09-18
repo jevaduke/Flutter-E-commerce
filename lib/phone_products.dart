0d02923da7a3a599bc23dc620fcead77857a7193
jevaduke <jevaduke@gmail.com>
2021-09-18 12:27:11 +0530

    s

--- lib/phone_products.dart
+++ lib/phone_products.dart
@@ -1,1 +1,32 @@
 import 'package:flutter/material.dart';
+
+class Iphone extends StatefulWidget {
+  const Iphone({Key? key}) : super(key: key);
+
+  @override
+  _IphoneState createState() => _IphoneState();
+}
+
+class _IphoneState extends State<Iphone> {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(slivers: [
+      SliverAppBar(
+        backgroundColor: Colors.lightBlue[900],
+        floating: true,
+        pinned: true,
+        snap: false,
+        centerTitle: false,
+        title: const Text('Mobile Phones'),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.filter_alt_rounded)),
+          IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    ]);
+  }
+}
